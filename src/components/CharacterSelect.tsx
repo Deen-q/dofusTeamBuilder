530b3969e414294c9e03b3d955a32319588fac84
Deen-q <124711768+Deen-q@users.noreply.github.com>
2024-11-24 19:56:24 +0000

    experimenting with onMouseEnter. currently displays all dpsRating, not per class

--- src/components/CharacterSelect.tsx
+++ src/components/CharacterSelect.tsx
@@ -4,13 +4,25 @@ import dofusClasses from "../data/dofusClasses";
 export default function CharacterSelect() {
     // console.log(">>>", dofusClasses);
 
+    //
+    const hoverButton = () => {
+        // alert("mouse entered")
+        const dps = dofusClasses.map((item) => (item.dpsRating));
+        alert(dps)
+    }
+
     return (
         <div>
             This is CharacterSelect!
             <div>
                 {/* {dofusClasses.map((index) => index.className)} */}
                 {dofusClasses.map((item, index) => (
-                    <div key={index}>{item.className}</div>
+                    <button
+                        key={index}
+                        onMouseEnter={hoverButton}
+                    >
+                        {item.className}
+                    </button>
                 ))}
             </div>
         </div>
