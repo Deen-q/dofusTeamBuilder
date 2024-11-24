8e1fc3c38d3291ae954a4f1d7c18cfdad2a3ee0c
Deen-q <124711768+Deen-q@users.noreply.github.com>
2024-11-24 19:41:14 +0000

    maps to page correctly. works without index for some reason?

--- src/components/CharacterSelect.tsx
+++ src/components/CharacterSelect.tsx
@@ -2,15 +2,16 @@
 import dofusClasses from "../data/dofusClasses";
 
 export default function CharacterSelect() {
-    console.log(">>>", dofusClasses);
-    const iop = dofusClasses[0]
+    // console.log(">>>", dofusClasses);
 
     return (
         <div>
             This is CharacterSelect!
             <div>
-                {/* Need .map method to prevent verbose code i.e., type out a div per class */}
-                Class 1: {iop.className}
+                {/* {dofusClasses.map((index) => index.className)} */}
+                {dofusClasses.map((item, index) => (
+                    <div key={index}>{item.className}</div>
+                ))}
             </div>
         </div>
     )
