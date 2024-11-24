8184b098d37d22099c1cbba6f764347eabece4a9
Deen-q <124711768+Deen-q@users.noreply.github.com>
2024-11-24 22:00:05 +0000

    messing around with onClick

--- src/components/CharacterSelect.tsx
+++ src/components/CharacterSelect.tsx
@@ -1,26 +1,33 @@
 /* Dofus Team Builder c: */
+import { useState } from "react";
 import dofusClasses from "../data/dofusClasses";
 
 export default function CharacterSelect() {
+    const [characterSelected, setCharacterSelected] = useState();
 
     const hoverButton = (x: number) => { // poor var name on purpose, to make a point!
-        alert(x)
+        console.log(x);
+    }
+
+    const handleClick = (y: any) => {
+        setCharacterSelected(y)
     }
 
     return (
         <div>
             This is CharacterSelect!
             <div>
-                {/* {dofusClasses.map((item) => item.className)} */}
                 {dofusClasses.map((item, index) => (
                     <button
                         key={index}
                         onMouseEnter={() => hoverButton(item.dpsRating)}
+                        onClick={() => handleClick(item.mobilityRating)}
                     >
                         {item.className}
                     </button>
                 ))}
             </div>
+            <div>Character's Mobility Score: {characterSelected}</div>
         </div>
     )
 }
