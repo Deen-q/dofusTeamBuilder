db4aa85aaf786c3fee951bb6b2f437d9f28c264b
Deen-q <124711768+Deen-q@users.noreply.github.com>
2024-11-24 22:06:55 +0000

    learning checkpoint comments

--- src/components/CharacterSelect.tsx
+++ src/components/CharacterSelect.tsx
@@ -3,13 +3,13 @@ import { useState } from "react";
 import dofusClasses from "../data/dofusClasses";
 
 export default function CharacterSelect() {
-    const [characterSelected, setCharacterSelected] = useState();
+    const [characterSelected, setCharacterSelected] = useState(Number()); // 3) characterSelected is item.mobilityRating
 
     const hoverButton = (x: number) => { // poor var name on purpose, to make a point!
         console.log(x);
     }
 
-    const handleClick = (y: any) => {
+    const handleClick = (y: number) => { // 2) y = item.mobilityRating
         setCharacterSelected(y)
     }
 
@@ -21,13 +21,13 @@ export default function CharacterSelect() {
                     <button
                         key={index}
                         onMouseEnter={() => hoverButton(item.dpsRating)}
-                        onClick={() => handleClick(item.mobilityRating)}
+                        onClick={() => handleClick(item.mobilityRating)} // 1) passes item.mobilityRating to handleClick
                     >
                         {item.className}
                     </button>
                 ))}
             </div>
-            <div>Character's Mobility Score: {characterSelected}</div>
+            <div>Character's Mobility Score: {characterSelected}</div> {/* 4) Bingo -> item.mobilityRating */}
         </div>
     )
 }
