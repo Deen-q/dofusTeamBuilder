dea140be768b6fdad178230db68bbe132a02175a
Deen-q <124711768+Deen-q@users.noreply.github.com>
2024-12-02 14:35:30 +0000

    better display of data

--- src/components/CharacterDisplay.tsx
+++ src/components/CharacterDisplay.tsx
@@ -1,5 +1,4 @@
 /* Dofus Team Builder c: */
-
 import { useContext } from "react"
 import CharacterContext from "../context/CharacterContext"
 
@@ -9,8 +8,18 @@ export default function CharacterDisplay() {
     return (
         <div>
             <h2>{characterSelected?.className}</h2> {/* fix ? if needed*/}
-            <p>Qualities: {characterSelected?.qualities.join(", ")}</p>
-            {/* Add MORE qualities here - maybe even rename Qualities to something else? */}
+            <h3>DPS Rating: {characterSelected?.dpsRating}</h3>
+            <h3>Healing Rating: {characterSelected?.healingRating}</h3>
+            {/* Likely need a Shield Point rating */}
+            <h3>Effective Range(s): {characterSelected?.effectiveRange}</h3>
+            <h3>Mobility Rating: {characterSelected?.mobilityRating}</h3>
+            <ul>
+                {characterSelected?.qualities.map((item) => (
+                    <li>
+                        {item}
+                    </li>
+                ))}
+            </ul>
         </div>
     )
 }
